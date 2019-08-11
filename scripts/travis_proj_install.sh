bd2ef4ccb699dc07d5c844bba963c1e0f1c83f39
René Buffat <buffat@gmail.com>
2019-08-11 21:22:45 +0200

    fix script

--- scripts/travis_proj_install.sh
+++ scripts/travis_proj_install.sh
@@ -21,14 +21,14 @@ ls -l $PROJINST
 
 echo "PROJ VERSION: $PROJVERSION"
 
-PROJ_DEB_PATH="proj_${PROJVERSION}-1_amd64_${DISTRIB_CODENAME}.deb"
-if ( curl -o/dev/null -sfI "https://rbuffat.github.io/gdal_builder/$PROJ_DEB_PATH" ); then
-    We install proj deb if available
-
-    wget "https://rbuffat.github.io/gdal_builder/$PROJ_DEB_PATH"
-    sudo dpkg -i "$PROJ_DEB_PATH"
-
-else
+# PROJ_DEB_PATH="proj_${PROJVERSION}-1_amd64_${DISTRIB_CODENAME}.deb"
+# if ( curl -o/dev/null -sfI "https://rbuffat.github.io/gdal_builder/$PROJ_DEB_PATH" ); then
+#     We install proj deb if available
+# 
+#     wget "https://rbuffat.github.io/gdal_builder/$PROJ_DEB_PATH"
+#     sudo dpkg -i "$PROJ_DEB_PATH"
+# 
+# else
 # Otherwise we compile proj from source
 
 if [ ! -d "$PROJINST/proj-$PROJVERSION" ]; then
