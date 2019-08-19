bf67c9d8b2d6adf3258557b275e4800cd433262e
René Buffat <buffat@gmail.com>
2019-08-19 22:14:15 +0200

    switch to debs that bundle gdal and proj

--- scripts/travis_gdal_install.sh
+++ scripts/travis_gdal_install.sh
@@ -63,14 +63,14 @@ fi
 
 ls -l $GDALINST
 
-GDAL_DEB_PATH="gdal_${GDALVERSION}_proj_${PROJVERSION}-1_amd64_${DISTRIB_CODENAME}.deb"
+GDAL_DEB_PATH="gdal_${GDALVERSION}_proj_${PROJVERSION}_${DISTRIB_CODENAME}.deb"
 if ( curl -o/dev/null -sfI "https://rbuffat.github.io/gdal_builder/$GDAL_DEB_PATH" ); then
 #   install deb when available
   
   wget "https://rbuffat.github.io/gdal_builder/$GDAL_DEB_PATH"
   sudo dpkg -i "$GDAL_DEB_PATH"
   
-  sudo chown -R root:root $GDALINST
+  sudo chown -R travis:travis $GDALINST
 
 elif [ "$GDALVERSION" = "master" ]; then
     PROJOPT="--with-proj=$PROJINST/proj-$PROJVERSION"
--- scripts/travis_proj_install.sh
+++ scripts/travis_proj_install.sh
@@ -4,51 +4,40 @@ set -e
 # change back to travis build dir
 cd $TRAVIS_BUILD_DIR
 
-# TODO debug
-rm -rf $PROJBUILD
-rm -rf $PROJINST
-
 # Create build dir if not exists
 if [ ! -d "$PROJBUILD" ]; then
     mkdir $PROJBUILD;
 fi
 
-if [ ! -d "$PROJINST" ]; then
-    mkdir $PROJINST;
+if [ ! -d "$GDALINST" ]; then
+    mkdir $GDALINST;
 fi
 
-ls -l $PROJINST
 
 echo "PROJ VERSION: $PROJVERSION"
 
-PROJ_DEB_PATH="proj_${PROJVERSION}-1_amd64_${DISTRIB_CODENAME}.deb"
-if ( curl -o/dev/null -sfI "https://rbuffat.github.io/gdal_builder/$PROJ_DEB_PATH" ); then
-#     We install proj deb if available
-
-    wget "https://rbuffat.github.io/gdal_builder/$PROJ_DEB_PATH"
-    sudo dpkg -i "$PROJ_DEB_PATH"
-    
-    sudo chown -R root:root $PROJINST
+GDAL_DEB_PATH="gdal_${GDALVERSION}_proj_${PROJVERSION}_${DISTRIB_CODENAME}.deb"
+if ( curl -o/dev/null -sfI "https://rbuffat.github.io/gdal_builder/$GDAL_DEB_PATH" ); then
+#     We do nothing if deb is available
+    echo "deb available, skip installation of proj"
 
 else
 
 # Otherwise we compile proj from source
 
-    if [ ! -d "$PROJINST/proj-$PROJVERSION" ]; then
+    if [ ! -d "$PROJINST/gdal-$GDALVERSION/share/proj" ]; then
         cd $PROJBUILD
 
         wget -q http://download.osgeo.org/proj/proj-$PROJVERSION.tar.gz
         tar -xzf proj-$PROJVERSION.tar.gz
         cd proj-$PROJVERSION
-        ./configure --prefix=$PROJINST/proj-$PROJVERSION
+        ./configure --prefix=$GDALINST/gdal-$GDALVERSION
         make -j 2
         make install
         rm -rf $PROJBUILD
     fi
 
 fi
-
-ls -l $PROJINST
 
 # change back to travis build dir
 cd $TRAVIS_BUILD_DIR
