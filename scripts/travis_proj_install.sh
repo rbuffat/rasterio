e6ee77459382b48a04fbda786df53df3a7d32c75
René Buffat <buffat@gmail.com>
2019-08-20 22:03:11 +0200

    comp

--- scripts/travis_proj_install.sh
+++ scripts/travis_proj_install.sh
@@ -17,7 +17,7 @@ fi
 echo "PROJ VERSION: $PROJVERSION FORCE_GDAL_BUILD: $FORCE_GDAL_BUILD" 
 
 GDAL_DEB_PATH="gdal_${GDALVERSION}_proj_${PROJVERSION}_${DISTRIB_CODENAME}.deb"
-if ( curl -o/dev/null -sfI "https://rbuffat.github.io/gdal_builder/$GDAL_DEB_PATH" ) && [ $FORCE_GDAL_BUILD!="yes" ]; then
+if ( curl -o/dev/null -sfI "https://rbuffat.github.io/gdal_builder/$GDAL_DEB_PATH" ) && [ "$FORCE_GDAL_BUILD" != "yes" ]; then
 #     We do nothing if deb is available
     echo "deb available, skip installation of proj"
 
@@ -25,7 +25,7 @@ else
 
 # Otherwise we compile proj from source
 
-    if [ ! -d "$PROJINST/gdal-$GDALVERSION/share/proj" ] || [ $FORCE_GDAL_BUILD="yes" ]; then
+    if [ ! -d "$PROJINST/gdal-$GDALVERSION/share/proj" ] || [ "$FORCE_GDAL_BUILD"="yes" ]; then
         cd $PROJBUILD
 
         wget -q http://download.osgeo.org/proj/proj-$PROJVERSION.tar.gz
